dc5fb6fa1b976dbfa7f0f94118c2799f510b3387
sohel <syed.sohel@learnbay.co>
2025-07-29 15:28:34 +0530

    twicked the UI with responsiveness

--- frontend/src/components/RegisterNewUser/RegisterNewUser.jsx
+++ frontend/src/components/RegisterNewUser/RegisterNewUser.jsx
@@ -3,6 +3,7 @@ import styles from "./RegisterNewUser.module.css";
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { FaWhatsappSquare } from "react-icons/fa";
 import Image from "next/image";
 
 const RegisterNewUser = () => {
@@ -39,13 +40,22 @@ const RegisterNewUser = () => {
     setError("");
     setStatusError("");
     setFieldErrors({});
+    setLoading(true);
+
     try {
       const data = await axios.post(
         `${process.env.NEXT_PUBLIC_API_URL}/api/register`,
         formData
       );
-    } catch (error) {}
-    setIsSuccess(true);
+      setIsSuccess(data?.data?.success);
+      setLoading(false);
+    } catch (error) {
+      setError(
+        error?.response?.data?.error ||
+          "Something went wrong! Please try again."
+      );
+      setLoading(false);
+    }
   };
 
   const handleLogin = () => {
@@ -204,6 +214,9 @@ const RegisterNewUser = () => {
             <motion.div>
               <motion.div className={styles.stepContent}>
                 <motion.form onSubmit={handleSubmit}>
+                  <motion.div>
+                    {error && <p className={styles.error}>{error}</p>}
+                  </motion.div>
                   <motion.h2 className={styles.title} variants={itemVariants}>
                     Start Your Application{" "}
                   </motion.h2>
@@ -235,7 +248,7 @@ const RegisterNewUser = () => {
                       type="number"
                       name="mobile"
                       placeholder="Your Mobile"
-                      value={formData.mobile}
+                      value={formData?.mobile}
                       onChange={handleChange}
                     />
                   </motion.div>
@@ -261,17 +274,19 @@ const RegisterNewUser = () => {
                       ))}
                     </motion.select>
                   </motion.div>
-
-                  <motion.button
-                    type="submit"
-                    className={styles.nextBtn}
-                    style={{ marginTop: "20px" }}
-                  >
-                    Submit
-                  </motion.button>
+                  <motion.div className={styles.center}>
+                    <motion.button
+                      type="submit"
+                      className={styles.button}
+                      whileHover={{ scale: 1.03 }}
+                      whileTap={{ scale: 0.98 }}
+                      variants={itemVariants}
+                      disabled={loading}
+                    >
+                      Submit
+                    </motion.button>
+                  </motion.div>
                 </motion.form>
-
-                {error && <p className={styles.error}>{error}</p>}
               </motion.div>
             </motion.div>
           ) : (
@@ -285,16 +300,66 @@ const RegisterNewUser = () => {
                 <motion.h2 className={styles.step}>
                   Your Registration successful please login
                 </motion.h2>
+                <motion.div className={styles.center}>
+                  <motion.button
+                    className={styles.button}
+                    onClick={handleLogin}
+                    whileHover={{ scale: 1.03 }}
+                    whileTap={{ scale: 0.98 }}
+                    variants={itemVariants}
+                  >
+                    Login Now
+                  </motion.button>
+                </motion.div>
+                <motion.p className={styles.features} variants={itemVariants}>
+                  Unlock your Personalized Dashboard, Evaluation Form, Screening
+                  Access, and Program details
+                </motion.p>
 
-                <motion.button
-                  className={styles.button}
-                  onClick={handleLogin}
-                  whileHover={{ scale: 1.03 }}
-                  whileTap={{ scale: 0.98 }}
-                  variants={itemVariants}
-                >
-                  Login Now
-                </motion.button>
+                <motion.div className={styles.contact} variants={itemVariants}>
+                  <p>
+                    Still Need Help?{" "}
+                    <a
+                      href="https://api.whatsapp.com/send?phone=917349222263"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        window.open(
+                          "https://api.whatsapp.com/send?phone=917349222263",
+                          "_blank"
+                        );
+                      }}
+                    >
+                      Connect now
+                    </a>
+                  </p>
+
+                  <div className={styles.contactOptions}>
+                    <motion.div
+                      className={styles.calendlyImage}
+                      whileHover={{ scale: 1.05 }}
+                    >
+                      <a href="https://calendly.com/" target="_blank">
+                        <Image
+                          src="/Calendly.png"
+                          alt="Calendly"
+                          width={130}
+                          height={60}
+                          loading="lazy"
+                        />
+                      </a>
+                    </motion.div>
+
+                    <motion.a
+                      href="https://api.whatsapp.com/send?phone=917349222263"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className={styles.whatsappLink}
+                      whileHover={{ scale: 1.1 }}
+                    >
+                      <FaWhatsappSquare className={styles.whatsappIcon} />
+                    </motion.a>
+                  </div>
+                </motion.div>
               </motion.div>
             </motion.div>
           )}
